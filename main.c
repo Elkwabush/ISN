faf1901cebd774163eb28b0f44d381d3719443fa
Elkwabush <Elkwabush@users.noreply.github.com>
2017-02-18 07:28:29 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+// ############################################# //
+//                                               //
+// Tristan PREVOST                               //
+// Maxime BRECQUEVILLE                           //
+// Victor COTTIN                                 //
+//                                               //
+// Dernière modification : 18.02.2017            //
+//                                               //
+// ############################################# //
+
+#include "graphics.h"
+
+int main (int argc, char** argv)
+{
+    exit(0) ;
+}
